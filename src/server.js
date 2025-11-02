20cc08d2b2dcb23ca9c126cc18ccde4d13fbb1a0
Jitendra <jitendrakumarbindra760@gmail.com>
2025-11-02 01:44:54 -0700

    wwww

--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
 import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
+// Root route FIRST
+app.get('/', (req, res) => {
+  res.send('🚀 API is running...');
+});
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
