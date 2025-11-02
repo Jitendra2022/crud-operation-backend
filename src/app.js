0720bb447f42746a38dbee5e0cd9aa39d31844cd
Jitendra <jitendrakumarbindra760@gmail.com>
2025-11-02 01:32:11 -0700

    wwqq

--- src/app.js
+++ src/app.js
@@ -2,19 +2,20 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import  connectDB  from "./config/db.js";
+import connectDB from "./config/db.js";
 import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+
 
 // Connect DB before defining routes (important)
 connectDB()
   .then(() => console.log('✅ MongoDB connected'))
   .catch((err) => console.error('❌ MongoDB connection error:', err.message));
+app.use(express.json());
 
 app.use("/api/users", userRoutes);
 // Root route
